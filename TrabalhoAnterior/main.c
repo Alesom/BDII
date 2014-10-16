8b5665201eeb782c9c5850178fdc56ce8caa38a1
Alesom <alesom.zorzi@gmail.com>
2014-10-16 18:13:18 -0300

    FINAL

--- TrabalhoAnterior/main.c
+++ TrabalhoAnterior/main.c
@@ -1,3 +1,8 @@
+/*Códigos feitos por:
+ * Elias Fank
+ * Alesom Zorzi
+ * João Gehlen
+ * */
 //só compilar gcc *.c -o main -Wall
 //formato de chamada ./main -t <nome_tabela> <atributo> <tipo> <tamanho> ...
 //exemplo de chamada: ./main -t pessoa nome S 20 idade I 2 -t endereco rua S 20 numero S 20
@@ -5,11 +10,14 @@
 //Obs: caso sair do programa, para executar novamente, chame com todos os parametros de criação de tabelas:
 							//se chamou ./main -t pessoa nome S 20 idade I 2 -t endereco rua S 20 numero S 20 na primeira vez
 							//chame assim na segunda e nao apenas ./main
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "buffend.h"
+
+#define MAX 11234
 
 typedef struct _atributo{
 	char *nome;
@@ -17,16 +25,19 @@ typedef struct _atributo{
 	int tamanho;
 	struct _atributo *next;
 }Atributo;
+
 typedef struct _listaatributos{
 	int quantidade;
 	Atributo *first;
 	Atributo *last;
 }Atributos;
+
 typedef struct _tabela{
 	char *nome;
 	Atributos *atributos;
 	struct _tabela *next;
 }Table;
+
 typedef struct _listatabelas{
 	int quantidade;
 	Table *first;
@@ -38,6 +49,7 @@ void inicializalistatabelas(TableList *tabelas){
 	tabelas->first=NULL;
 	tabelas->last=NULL;
 }
+
 void inicializalistaatributos(Atributos *atributos){
 	atributos->quantidade=0;
 	atributos->first=NULL;
@@ -76,7 +88,7 @@ void mostrartabelascomatributos(TableList *tabelas){
 		auxatri=aux->atributos->first;
 		printf("|%s|\n", aux->nome);
 		while(auxatri!=NULL){
-			printf("%s(%d-%c)\n",auxatri->nome, auxatri->tamanho, auxatri->tipo);
+			printf("%s(%c-%d)\n",auxatri->nome, auxatri->tipo, auxatri->tamanho);
 			auxatri=auxatri->next;
 		}
 		aux=aux->next;
@@ -92,7 +104,6 @@ void criartabelas(TableList *tabelas){
 		auxatri=aux->atributos->first;
 		t = iniciaTabela(aux->nome);
 		if(t == NULL){
-			printf("Erro: na função iniciaTabela(). Nome da tabela já existente.\n");
 			return;
 		}
 		while(auxatri!=NULL){
@@ -126,6 +137,7 @@ void mostraropcoes(TableList *tabelas){
 	}
 	return;
 }
+
 void inserirtupla(char *tabela, TableList *tabelas){
 	int erro;
 	column *c = NULL;
@@ -140,38 +152,37 @@ void inserirtupla(char *tabela, TableList *tabelas){
 			auxatri=aux->atributos->first;
 			while(auxatri!=NULL){
 				printf("Insira %s(%d-%c):\n",auxatri->nome, auxatri->tamanho, auxatri->tipo);
-						if(auxatri->tipo == 'C'){
-							scanf("%c", &caracter);
-							c = insereValor(c, auxatri->nome, &caracter);
-						}
-						if(auxatri->tipo == 'I'){
-							scanf("%d", &inteiro);
-							sprintf(string, "%i", inteiro);  
-							c = insereValor(c, auxatri->nome, string);
-						}
-						if(auxatri->tipo == 'D'){
-							scanf("%lf", &doublevar);
-							sprintf(string, "%lf", doublevar); 
-							c = insereValor(c, auxatri->nome, string);
-						}
-						if(auxatri->tipo == 'S'){
-							scanf("%s", string);
-							c = insereValor(c, auxatri->nome, string);
-						}
-				
+				if(auxatri->tipo == 'C'){
+					scanf("%c", &caracter);
+					c = insereValor(c, auxatri->nome, &caracter);
+				}else if(auxatri->tipo == 'I'){
+					scanf("%d", &inteiro);
+					sprintf(string, "%i", inteiro);  
+					c = insereValor(c, auxatri->nome, string);
+				}else if(auxatri->tipo == 'D'){
+					scanf("%lf", &doublevar);
+					sprintf(string, "%lf", doublevar); 
+					c = insereValor(c, auxatri->nome, string);
+				}else if(auxatri->tipo == 'S'){
+					scanf("%s", string);
+					c = insereValor(c, auxatri->nome, string);
+				}
+
 				auxatri=auxatri->next;
 			}
 			erro = finalizaInsert(tabela, c);
-				if(erro != SUCCESS){
-					printf("Erro %d: na função finalizaInsert()\n", erro);
-					return ;
-				}
+			if(erro != SUCCESS){
+				printf("Erro %d: na função finalizaInsert()\n", erro);
+				return ;
+			}
 		}
 		aux=aux->next;
 	}
 	
 }
 void mostrartabelasfinal(TableList *tabelas){
+	printf("\n");
+	printf("TABELAS\n");
 	int erro,x,flag;
 	Table *aux;
 	aux = tabelas->first;
@@ -218,19 +229,74 @@ void mostrartabelasfinal(TableList *tabelas){
 			
 		aux=aux->next;
 		}
+		printf("\n");
 		return;
 	}
+void le(){
+	char B[MAX];
+	int tam;
+
+	FILE *arq=fopen("inicialize.dat", "r+");
+	if (arq!=NULL){
+		printf("cadeeeente\n");
+		fseek(arq, 0, SEEK_SET);
+		while(!feof(arq)){
+			fread(&tam, sizeof(int), 1, arq);
+			fread(B, sizeof(char), tam+1, arq);
+			B[tam]='\0';
+			printf("%d %s\n", tam, B);
+		}
+	}
+	fclose(arq);
+}
+
 int  main(int argc, char **argv){
-	int i;
+	char Menu;
+	int x;
 	char t[2];
 	TableList *tabelas=(TableList *) malloc (sizeof(TableList));
 	Table *novatabela=NULL;
 	Atributo *novoatributo=NULL;
+	FILE *arq; //bloco feito por ultimo
+	int i, tam;
+	
+	system("clear");
+	arq=fopen("inicialize.dat", "r+");
+	
+	if (arq==NULL ){
+		if (argc < 2) {
+			printf("Aconteceu um erro grave! Verifique se os parâmetros da execução foram passados corretamente\n");
+			return 0;
+		}
+		arq=fopen("inicialize.dat", "w+");
+		for (i=1;i<argc-1;i++){
+			tam=strlen(argv[i]);
+			fwrite(&tam, sizeof(int), 1, arq);
+			fwrite(argv[i], sizeof(char), tam, arq);
+			fwrite(" ", 1, 1, arq);
+		}
+		fclose(arq);
+	}else{
+		fseek(arq, 0, SEEK_SET);
+		i=0;
+		argv=malloc(sizeof(char *) * MAX);
+		for (i=1; !feof(arq);i++){
+			fread(&tam, sizeof(int), 1, arq);
+			argv[i] = malloc(sizeof(char) * (tam+1));
+			fread(argv[i], sizeof(char), tam+1, arq);
+			argv[i][tam]='\0';
+		}
+		argc=i;
+		fclose(arq);
+	}
+
 	inicializalistatabelas(tabelas);
 	strcpy(t,"-t");
-	printf("%d\n",argc);
-	for (i=1; i<argc-1;i++){
+	printf("O seu banco tem as sequintes tabelas:\n\n");
+	for (i=1; i<argc;i++){
 		if(strncmp(argv[i], t, 2)==0){
+			if (i!=1)
+				printf("\n\n\n");
 			printf("Tabela: %s",argv[i+1]);
 			novatabela = (Table *) malloc (sizeof(Table));
 			novatabela->nome = (char*)malloc(strlen(argv[i+1])*sizeof(char));
@@ -240,47 +306,56 @@ int  main(int argc, char **argv){
 			inicializalistaatributos(novatabela->atributos);
 			i++;
 		}else{
-				novoatributo = (Atributo *) malloc (sizeof(Atributo));
-				
-				printf("Atributo: %s ", argv[i]);
-				novoatributo->nome = (char *) malloc (strlen(argv[i])*sizeof(char));
-				strcpy(novoatributo->nome, argv[i]);
-				i++;
-				printf("Tipo: %c ", argv[i][0]);
-				novoatributo->tipo= argv[i][0];
-				i++;
-				printf("Tamanho: %s\n", argv[i]);
-				novoatributo->tamanho=atoi(argv[i]);
-				insereatributo(novatabela->atributos, novoatributo);
-			}
-			if((i+1)>=argc || strncmp(argv[i+1], t, 2)==0)
+			novoatributo = (Atributo *) malloc (sizeof(Atributo));
+			printf("Atributo: %s ", argv[i]);
+			novoatributo->nome = (char *) malloc (strlen(argv[i])*sizeof(char));
+			strcpy(novoatributo->nome, argv[i]);
+			i++;
+			printf("Tipo: %c ", argv[i][0]);
+			novoatributo->tipo= argv[i][0];
+			i++;
+			printf("Tamanho: %s\n", argv[i]);
+			novoatributo->tamanho=atoi(argv[i]);
+			insereatributo(novatabela->atributos, novoatributo);
+		}
+		if((i+1)>=argc || strncmp(argv[i+1], t, 2)==0)
 			inseretabela(tabelas, novatabela);
+		printf("\n");
 	}
 	
-	mostrartabelascomatributos(tabelas);
-	
 	criartabelas(tabelas);
-	
-	int x = 1;
+	printf("Deseja ir para o menu?\n");
+	printf("[s/n]\n");
+	scanf("%c", &Menu);
+	if (Menu=='s'){
+		x=1;
+	}else
+		x=4;
+
 	char tabela[100];
-	while(x!=3){
+	while(x!=4){
 		printf("MENU\n");
-		printf("1-inserir tupla\n");
-		printf("2-mostrar tabelas\n");
-		printf("3-Sair\n");
+		printf("1-Inserir tupla\n");
+		printf("2-Mostrar tabelas\n");
+		printf("3-Deletar o Banco de Dados e sair\n");
+		printf("4-Sair\n");
 		scanf("%d",&x);
 		if(x == 1){
 			printf("Em qual tabela deseja adicionar: ");
 			scanf("%s",tabela);
 			inserirtupla(tabela, tabelas);
-		}
-		if(x == 2){
-			mostrartabelasfinal(tabelas);
-			
+		}else	if(x == 2){
+			mostrartabelasfinal(tabelas);	
+		}else if (x==3){
+			printf("Tem certeza que deseja excluir o banco? [s/n]\n");
+			getchar();
+			scanf("%c", &Menu);
+			if (Menu=='s'){
+				system("rm -f *.dat");
+				x=4;
+			}
 		}
 	}
-	
-
 	printf("\n");
 	return 0;
 }
